b5fc39499b66471d4997b0b70e8012d0ec87b737
Martcelius <martcel.martcelius@gmail.com>
2018-01-18 19:44:19 +0700

    templating ubilearn

---
+++ application/views/layout/script.php
@@ -0,0 +1,10 @@
+<script src="<?php echo base_url(); ?>res/assets/js/d3.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/getmdl-select.min.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/material.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/nv.d3.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/employer-form/employer-form.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/line-chart/line-chart-nvd3.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/map/maps.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/pie-chart/pie-chart-nvd3.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/table/table.js"></script>
+<script src="<?php echo base_url(); ?>res/assets/js/widgets/todo/todo.js"></script>
---
+++ application/views/layout/sidebar.php
@@ -0,0 +1,25 @@
+<header>darkboard</header>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link mdl-navigation__link--current" href="index.html">
+                <i class="material-icons" role="presentation">dashboard</i>
+                Dashboard
+            </a>
+            <a class="mdl-navigation__link" href="forms.html">
+                <i class="material-icons" role="presentation">person</i>
+                Account
+            </a>
+            <a class="mdl-navigation__link" href="maps.html">
+                <i class="material-icons" role="presentation">map</i>
+                Maps
+            </a>
+            <a class="mdl-navigation__link" href="ui.html">
+                <i class="material-icons">view_comfy</i>
+                UI Elements
+            </a>
+
+            <div class="mdl-layout-spacer"></div>
+            <a class="mdl-navigation__link" href="https://github.com/CreativeIT/getmdl-dashboard">
+                <i class="material-icons" role="presentation">link</i>
+                GitHub
+            </a>
+        </nav>
