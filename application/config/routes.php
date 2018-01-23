cb15af42d9e9c300809c964478c674bb03923ae1
Adzan Nugraha <adzan.nugraha@gmail.com>
2018-01-23 19:11:22 +0700

    course_close

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,8 +56,11 @@ $route['translate_uri_dashes'] = FALSE;
 // route siswa
 $route['siswa/dashboard'] = 'C_siswa/dashboard_siswa';
 $route['siswa/akun'] = 'C_siswa/manage_akun';
+$route['siswa/course_close'] = 'C_siswa/course_close';
 
 // route login
 
 $route['signin'] = 'welcome/signin';
 $route['signup'] = 'welcome/signup';
+
+
