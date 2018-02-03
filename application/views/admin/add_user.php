d254b0bc92b721000d780395909ccd49620bdf77
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-02-03 15:37:23 +0700

    add_user

--- application/views/admin/add_user.php
+++ application/views/admin/add_user.php
@@ -1,90 +1,86 @@
 <main class="mdl-layout__content">
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-        <main class="mdl-layout__content mdl-color--grey-100">
-            <div class="mdl-card mdl-shadow--2dp employer-form" action="#">
-                <div class="mdl-card__title">
-                    <h2>Tambah User</h2>
-                    <div class="mdl-card__subtitle">Masukan Detail User</div>
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--8-col-tablet mdl-cell--2-offset-tablet mdl-cell--12-col-phone">
+            <div class="mdl-card mdl-shadow--2dp pie-chart">
+                <div class="mdl-card__title" style="display:block;">
+                    <h2 class="mdl-card__title-text">Tambah User</h2>
+                    <div class="mdl-card__subtitle-text">Masukan Detail User</div>
                 </div>
-
                 <div class="mdl-card__supporting-text">
-                    <form action="#" class="form">
-                        <div class="form__article">
-                            <div class="mdl-grid">
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="text" id="firstName" placeholder="Nama Depan"/>
-                                    <label class="mdl-textfield__label" for="firstName">Nama Depan</label>
-                                </div>
-
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="text" id="secondName" placeholder="Nama Belakang"/>
-                                    <label class="mdl-textfield__label" for="secondName">Nama Belakang</label>
-                                </div>
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="username" class="col-sm-2 control-label" style="font-size: 14px">Username</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="username" placeholder="Username">
                             </div>
-
-                            <div class="mdl-grid">
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="date" id="birthday" placeholder="Tanggal lahir"/>
-                                    <label class="mdl-textfield__label" for="birthday">Tanggal Lahir</label>
+                            <label for="password" class="col-sm-2 control-label" style="font-size: 14px">Password</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="namaDepan" class="col-sm-2 control-label" style="font-size: 14px">Nama Depan</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="namaDepan" placeholder="Nama Depan">
+                            </div>
+                            <label for="namaBelakang" class="col-sm-2 control-label" style="font-size: 14px">Nama Belakang</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="namaBelakang" placeholder="Nama Belakang">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="col-sm-2 control-label" style="font-size: 14px">Email</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" id="email" placeholder="E-mail">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="jenis_kelamin" class="col-sm-2 control-label" style="font-size: 14px">Jenis Kelamin</label>
+                            <div class="col-sm-5">
+                                <div class="radio-inline">
+                                    <label style="font-size: 14px"><input type="radio" name="optradio">Laki-laki</label>
                                 </div>
-
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
-                                    <input class="mdl-textfield__input" placeholder="Jenis Kelamin" type="text" id="gender" readonly tabIndex="-1"/>
-
-                                    <label class="mdl-textfield__label" for="gender">Jenis Kelamin</label>
-
-                                    <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu dark_dropdown" for="gender">
-                                        <li class="mdl-menu__item">Pria</li>
-                                        <li class="mdl-menu__item">Wanita</li>
-                                    </ul>
-
-                                    <label for="gender">
-                                        <i class="mdl-icon-toggle__label material-icons">arrow_drop_down</i>
-                                    </label>
+                                <div class="radio-inline">
+                                    <label style="font-size: 14px"><input type="radio" name="optradio">Wanita</label>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form__article">
-                            <div class="mdl-grid">
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="text" placeholder="Username" id="username"/>
-                                    <label class="mdl-textfield__label" for="username">Username</label>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" style="font-size: 14px">Role</label>
+                            <div class="col-sm-5">
+                                <div class="radio-inline">
+                                    <label style="font-size: 14px"><input type="radio" name="optradio1">Admin</label>
                                 </div>
-                                <div class="mdl-cell mdl-cell--6-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="decimal" min="0" max="4" placeholder="IPK" id="gpa"/>
-                                    <label class="mdl-textfield__label" for="gpa">IPK</label>
+                                <div class="radio-inline">
+                                    <label style="font-size: 14px"><input type="radio" name="optradio1">Siswa</label>
+                                </div>
+                                <div class="radio-inline">
+                                    <label style="font-size: 14px"><input type="radio" name="optradio1">Instruktur</label>
                                 </div>
                             </div>
-
-                            <span>Role</span>
-
-                            <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="partition-fulltime">
-                                <input type="radio" id="partition-fulltime" class="mdl-radio__button" name="employment" value="1" checked/>
-                                <span class="mdl-radio__label">Admin</span>
-                            </label>
-                            <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="partition-partTime">
-                                <input type="radio" id="partition-partTime" class="mdl-radio__button" name="employment" value="2"/>
-                                <span class="mdl-radio__label">Siswa</span>
-                            </label>
-                            <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="partition-remote">
-                                <input type="radio" id="partition-remote" class="mdl-radio__button" name="employment" value="3"/>
-                                <span class="mdl-radio__label">Instruktur</span>
-                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" style="font-size: 14px">IPK</label>
+                            <div class="col-sm-4">
+                                <input type="decimal" min="0" max="4" class="form-control" id="ipk" placeholder="IPK">
+                            </div>
+                            <span>*Isi bila user adalah siswa</span>
                         </div>
 
-                        <div class="form__action">
-                            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="isInfoReliable">
-                                <input type="checkbox" id="isInfoReliable" class="mdl-checkbox__input" required/>
-                                <span class="mdl-checkbox__label" style="font-size: 10px">Dengan mencentang kolom disamping saya menyatakan bahwa informasi yang saya isi adalah benar.</span>
-                            </label>
-                            <button id="submit_button" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                                Submit
-                            </button>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" style="font-size: 14px">Upload gambar</label>
+                            <input type="file" id="gambar" accept="image/x-png,image/gif,image/jpeg" style="padding: 9px">
                         </div>
                     </form>
+                    <div class="form__action">
+                        <div class="mdl-layout-spacer"></div>
+                        <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored-blue">
+                            Selesai
+                        </button>
+                    </div>
                 </div>
             </div>
-        </main>
+        </div>
     </div>
 </main>
+
