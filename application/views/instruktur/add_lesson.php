d3a51e23dbae0c5a3b47f4e719fa39ac028188c1
fikrirazzaq <fikrirazzaq30@gmail.com>
2018-02-14 12:08:03 +0700

    AINYEINYE

--- application/views/instruktur/add_lesson.php
+++ application/views/instruktur/add_lesson.php
@@ -1,6 +1,15 @@
 <main class="mdl-layout__content">
 <div class="mdl-grid cover-main"></div>
     <div class="mdl-grid">
+
+    <?php if ($this->session->flashdata('data_tersimpan') == TRUE): ?>
+    <div role="alert"  class="alert alert-success alert-dismissible fade in mdl-cell mdl-cell--12-col-desktop mdl-cell--8-col-tablet mdl-cell--2-offset-tablet mdl-cell--12-col-phone"> 
+        <button aria-label="Close" data-dismiss="alert" class="close" type="button"><span aria-hidden="true" class="fa fa-times"></span>
+        </button>
+        <p><?php echo $this->session->flashdata('data_tersimpan')?></p>
+    </div>
+    <?php endif; ?>
+    
         <div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--8-col-tablet mdl-cell--2-offset-tablet mdl-cell--12-col-phone">
             <div class="mdl-card mdl-shadow--2dp pie-chart">
                 <div class="mdl-card__title" style="display:block;">
@@ -8,17 +17,17 @@
                     <div class="mdl-card__subtitle-text">Masukan Detail Lesson</div>
                 </div>
                 <div class="mdl-card__supporting-text">
-                    <form class="form-horizontal">
+                    <form class="form-horizontal" method="post" action="<?php echo site_url('instruktur/lesson/insert'.$id);?>">
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-label">Nama Lesson</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputEmail3" placeholder="Kode Course">
+                            <input name="lsn_name" type="text" class="form-control" id="inputEmail3" placeholder="Nama Lesson">
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputPassword3" class="col-sm-2 control-label">Intro Lesson</label>
                             <div class="col-sm-10">
-                            <textarea  class="form-control" id="inputPassword3" placeholder="Intro" rows="2"></textarea>
+                            <textarea name="lsn_intro" class="form-control" id="inputPassword3" placeholder="Intro Lesson" rows="2"></textarea>
                             </div>
                         </div>
                         <div class="form-group">
