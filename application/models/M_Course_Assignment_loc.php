3ca6f009a52fd44ac75973c3642d4fdcf9ec1771
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-03-17 21:11:42 +0700

    skenario_1

---
+++ application/models/M_Course_Assignment_loc.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class M_Course_Assignment_loc extends Eloquent
+{
+    protected $table = 'course_assignment_loc';
+    protected $primaryKey = 'id';
+    const CREATED_AT = NULL;
+    const UPDATED_AT = NULL;
+
+
+
+}
+
+?>
