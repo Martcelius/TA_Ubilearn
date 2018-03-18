a877809dc360a16417b997ec3fdf19772e69b8a1
Martcelius <martcel.martcelius@gmail.com>
2018-03-18 21:37:23 +0700

    input LOC pada add course

--- application/models/M_Course_Learning_Outcomes.php
+++ application/models/M_Course_Learning_Outcomes.php
@@ -10,12 +10,27 @@ class M_Course_Learning_Outcomes extends Eloquent
     const UPDATED_AT = 'loc_timemodified';
     protected $primaryKey = 'loc_id';
 
-    public function insert_lo($data)
+    public function insert_lo($lo,$crs_id)
     {
-        $lo = new M_Course_Learning_Outcomes;
-        $lo->loc_desc = $data['loc_desc'];
-        $lo->crs_id = $data['crs_id'];
-        return $lo ->save();
+        $jum = count($lo);
+        $i = 0;
+        while ($i < $jum){
+            $loc = new M_Course_Learning_Outcomes;
+            $loc->loc_desc = $lo[$i];
+            $loc->crs_id = $crs_id;
+            $stat = $loc->save();
+            $i++;
+        }
+//        $num = 0;
+//        foreach ($lo as $loc){
+//            $lo = new M_Course_Learning_Outcomes;
+//            $lo->loc_desc = $loc[$num];
+//            $lo->crs_id = $crs_id;
+//            $num++;
+//        }
+
+//        return $lo ->save();
+        return $stat;
     }
 
     public function update_lo($data)
