7319546dce538b122d17637913fc96793ddea34f
Martcelius <martcel.martcelius@gmail.com>
2018-03-19 17:07:34 +0700

    sidebar active & dashboard siswa

--- application/views/layout/sidebar_instruktur.php
+++ application/views/layout/sidebar_instruktur.php
@@ -1,18 +1,18 @@
 <header style="background:rgba(230, 230, 230, 0.93)"><img src="<?php echo site_url('res/assets/images/Ubilearning_logo.png')?>" style="height:50px ;width:500px ;"  alt="U-Learning"></header>
         <nav class="mdl-navigation">
-            <a class="mdl-navigation__link mdl-navigation__link--current" href="<?php echo site_url('instruktur/dashboard')?>">
+            <a class="mdl-navigation__link <?php if ($this->uri->segment(2) == 'dashboard') echo "mdl-navigation__link--current"?>" href="<?php echo site_url('instruktur/dashboard')?>">
                 <i class="material-icons" role="presentation">dashboard</i>
                 Dashboard
             </a>
-            <a class="mdl-navigation__link" href="<?php echo site_url('instruktur/MyCourse')?> ">
+            <a class="mdl-navigation__link <?php if ($this->uri->segment(2) == 'MyCourse') echo "mdl-navigation__link--current"?>" href="<?php echo site_url('instruktur/MyCourse')?> ">
                 <i class="material-icons" role="presentation">folder</i>
                My Course
             </a>
-            <a class="mdl-navigation__link" href="<?php echo site_url('instruktur/forum_instruktur')?>">
+            <a class="mdl-navigation__link <?php if ($this->uri->segment(2) == 'forum_instruktur') echo "mdl-navigation__link--current"?>" href="<?php echo site_url('instruktur/forum_instruktur')?>">
                 <i class="material-icons" role="presentation">forum</i>
                 Forum
             </a>
-            <a class="mdl-navigation__link" href="<?php echo site_url('instruktur/learning_outcome')?>">
+            <a class="mdl-navigation__link <?php if ($this->uri->segment(2) == 'learning_outcome') echo "mdl-navigation__link--current"?>" href="<?php echo site_url('instruktur/learning_outcome')?>">
                 <i class="material-icons" role="presentation">book</i>
                 Learning Outcome
             </a>
