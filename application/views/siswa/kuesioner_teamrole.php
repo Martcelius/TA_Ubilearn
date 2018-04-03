3648f1baffe229a45ddb88ffb796edfd60fd092f
Hanif <ilhamhanif1512@yahoo.co.id>
2018-04-03 16:31:03 +0700

    cek koneksi, nambah kuesioner

--- application/views/siswa/kuesioner_teamrole.php
+++ application/views/siswa/kuesioner_teamrole.php
@@ -22,7 +22,7 @@
                   <li>Form Kuesioner terletak di kolom sebelah kiri</li>
                   <li>Pada kolom Form Kuesioner, Klik "To The Test" atau scroll ke bawah sampai menemukan form untuk isi kuesioner</li>
                   <li>Setiap pertanyaan diberi 2 pilihan, Pilih lah yang paling cocok dengan kepribadianmu</li>
-                  <li>untuk "My Educational Level is" pilihlah "College" atau "University"</li>
+                  <li>untuk "My Educational Level is" pilihlah "University"</li>
                   <li>Klik tombol "next" jika sudah selesai mengisi semua pilihan</li>
                   <li>Centang hasil kuesioner kalian yang terdapat di kolom "Masukkan Hasil" yang terletak di sebelah kanan kolom kesioner</li>
                 </ol>
@@ -52,8 +52,119 @@
                 <h2 class="mdl-card__title-text">Masukkan Hasil</h2>
                 <div class="mdl-card__subtitle-text">Pilih Salah Satu</div>
             </div>
-            <div class="mdl-card__supporting-text" style="width: 100%;font-size: unset;">
-              
+            <div id="sub1" class="mdl-card__supporting-text" style="width: 100%;font-size: unset;">
+              <ul style="width: 100%;height:280px;overflow: auto;">
+                <strong><h4>Bantuan</h4></strong>
+                <li>Gregarious : Suka berkumpul/bergaul</li>
+                <li>Challenging : Menantang, suka tantangan</li>
+                <br/>
+                <li>Practical : Praktis, yang mudah dilaksanakan</li>
+                <li>Critical : Kritikal, suka memberi kritikan</li>
+                <br/>
+                <li>Orderly : Tertib, rapi</li>
+                <li>Communicative : Komunikatif, suka berbicara</li>
+                <br/>
+                <li>Curious : Selalu ingin tahu, penasaran</li>
+                <li>Respectful : Sopan</li>
+                <br/>
+                <li>Impatient : Tidak Sabar</li>
+                <li>Sensitive : Mudah tersinggung</li>
+                <br/>
+                <li>Sensible : Mudah mengerti, bijaksana</li>
+                <li>Agreeable : ramah, bersedia menyetujui pendapat lain</li>
+                <br/>
+                <li>Networking : Mudah berbagi/sharing </li>
+                <li>Controlling : Mengendalikan, mengontrol</li>
+                <br/>
+                <li>Systematic : Teratur menurut sistem, mengikuti aturan dengan baik</li>
+                <li>Diplomatic : Sangat berhati-hati dalam mengemukakan pendapat</li>
+                <br/>
+                <li>Extrovert : Senang bergaul, suka berinteraksi dengan orang lain, berorentasi kepada lingkungan sekitarnya</li>
+                <li>Independent : Senang menyendiri, lebih suka melakukan sesuatu sendiri</li>
+                <br/>
+                <li>Organize : Mengatur, mengorganisasi</li>
+                <li>Improvize : Suka berimprovisasi</li>
+                <br/>
+                <li>Concrete Actions : Bertindak sesuai dengan keadaan</li>
+                <li>Be Original : Bertindak sesuai diri sendiri</li>
+                <br/>
+                <li>Scientific : bertindak secara ilmiah</li>
+                <li>Impulsive : bersifat cepat bertindak secara tiba-tiba menurut gerak hati</li>
+                <br/>
+                <li>Fantasize : Berfantasi , berimajinasi</li>
+                <li>Convey : Menyampaikan, menngutarakan kepada orang lain</li>
+                <br/>
+                <li>Innovate : Memperbarui, mengadakan perubahan, berinovasi</li>
+                <li>Analyze : Menganalisis</li>
+                <br/>
+                <li>Ambitious : berambisi, berkeinginan keras mencapai sesuatu</li>
+                <li>Harmonious : selaras, secara harmonis</li>
+                <br/>
+                <li>Coordinating : Berkoordinasi</li>
+                <li>Autonomy : Mandiri</li>
+                <br/>
+                <li>Restless : Gelisah, Resah</li>
+                <li>Friendly : Ramah tamah</li>
+                <br/>
+                <li>Wide Interest : Minat yang luas</li>
+                <li>Avoid Conflict : Menghindari konflik</li>
+                <br/>
+                <li>Solidarity : Kesetiakawanan</li>
+                <li>Individualistic : Individual</li>
+                <br/>
+                <li>Sober : Bijaksana, membuat tenang</li>
+                <li>Emotional : Emosional, penuh emosi, menyentuh perasaan</li>
+                <br/>
+                <li>Visionary : Mengkhayal, hanya berangan-angan saja</li>
+                <li>Expert : Ahli</li>
+                <br/>
+                <li>Diligent : Rajin</li>
+                <li>Conscientious : Teliti, berhati-hati</li>
+                <br/>
+                <li>Disciplined : disiplin, patuh</li>
+                <li>Task-focused : Fokus terhadap apa yang dikerjakan</li>
+                <br/>
+                <li>Talk : Berbicara, bercakap</li>
+                <li>Study : Belajar, mempelajari</li>
+                <br/>
+                <li>Prudent : Bijaksana, hati-hati</li>
+                <li>Driven :Mendorong, membangkitkan</li>
+                <br/>
+                <li>Objective : Memiliki tujuan</li>
+                <li>Specialist : memilih yang spesialis/ahli dalam bidang tertentu</li>
+                <br/>
+                <li>Intelligent : cerdas, pandai</li>
+                <li>Worisome : khuatir, cemas</li>
+                <br/>
+                <li>Serious : Serius</li>
+                <li>Open-minded : Berpikiran secara terbuka</li>
+                <br/>
+                <li>Cheerful : riang, gembira</li>
+                <li>Reserved : pendiam</li>
+                <br/>
+                <li>Rational : rasional/masuk akal, pemikiran logis</li>
+                <li>Concerned : pemikiran berdasarkan perasaan</li>
+                <br/>
+                <li>Ambitious : berambisi, melakukan diluar kebiasaan</li>
+                <li>Stick to procedures : tetap pada prosedur</li>
+                <br/>
+                <li>Energetic : lebih memanfaatkan keaktifan</li>
+                <li>Skillful : lebih memanfaatkan kecakapan</li>
+                <br/>
+                <li>Bring Together : lebih suka menanggung bersama sama</li>
+                <li>Being Responsible : bertanggung jawab pada diri sendiri</li>
+                <br/>
+                <li>Consensus : pemufakatan, diskusi</li>
+                <li>Caring : lebih memperhatikan, pengamat</li>
+                <br/>
+                <li>Troubled : sikap lebih bebas</li>
+                <li>Defensive : sikap lebih pembelaan</li>
+                <br/>
+                <li>Imagine : membayangkan</li>
+                <li>Listen : mendengarkan</li>
+                <br/>
+              </ul>
+              <hr/>
               <ul>
                 <li>Hasil kuesioner ditunjukan pada pie-chart yang muncul</li>
                 <li>Centang hasil kuesioner yang sudah didapatkan, dibawah ini :</li>
@@ -76,12 +187,73 @@
                         <label class="radio"><input type="radio" name="hasil" value="Expert" required>Expert</label>
                       </div>
                     </div>
-                    <button type="submit" class="mdl-button mdl-js-button mdl-button--raised pull-right" style="background: #4a95b9;">Kirim</button>
-                </form>
+                    <button type="button" class="mdl-button mdl-js-button mdl-button--raised pull-right" onclick="return next()" style="background: #4a95b9;">Selanjutnya</button>
                 </li>
               </ul>
               
             </div>
+            <div id="sub2" class="mdl-card__supporting-text" style="width: 100%;font-size: unset;">
+            <ul>
+              <li style="list-style-type: none;">Pilih interest option dibawah ini yang cocok dengan anda (bisa hobi ato minat) Pilih salah satu saja</li><br/>
+              <li style="list-style-type: none;">
+                <div class="row">
+                  <div class="col-md-12">
+                     <div class="form-group">
+                      <select class="form-control" id="pilMinat" name="pilMinat">
+                        <option disabled selected>--Pilih Peminatan--</option>
+                        <option>Akademik</option>
+                        <option>Non-Akademik</option>
+                      </select>
+                    </div>  
+                  </div>
+                  <span id="Non">
+                    <div class="col-md-4">
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Sepak Bola" required>Sepak Bola</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Basket" required>Basket</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Voli" required>Voli</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Renang" required>Renang</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Lari/Jogging" required>Lari/Jogging</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Sepeda" required>Bersepeda</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Olahhraga Ekstrim" required>Olahraga Ekstrim</label>
+                    </div>
+                    <div class="col-md-4">
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Bermain Gitar" required>Bermain Gitar</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Bernyanyi" required>Bernyanyi</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Bermain Bass" required>Bermain Bass</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Bermain Drum" required>Bermain Drum</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Bermain Piano" required>Bermain Piano</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Menggambar/Melukis" required>Menggambar</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Fotografi" required>Fotografi</label>
+                    </div>
+                    <div class="col-md-4">
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Membaca Buku" required>Membaca Buku</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Otomotif" required>Otomotif</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Memasak" required>Memasak</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Travelling" required>Travelling</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Action Figure" required>Action Figure</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Gamer" required>Gamer</label>
+                      <label class="radio"><input type="radio" class="minat" name="minat" value="Menulis" required>Menulis</label>
+                    </div>
+                    tidak menemukan peminatan yang sesuai ?<br/>
+                    <button type="button" onclick="klikLain()" class="mdl-button mdl-js-button mdl-button--raised">klik disini untuk lainnya</button>
+                    <div class="col-md-12">
+                      <label class="radio lain"><input type="radio" class="minatLain lain2" name="minat" value="Olahraga" >Bidang Olahraga</label>
+                      <label class="radio lain"><input type="radio" class="minatLain lain2" name="minat" value="Musik" >Musik</label>
+                      <label class="radio lain"><input type="radio" class="minatLain lain2" name="minat" value="Seni" >Seni</label>
+                      <label class="radio lain"><input type="radio" class="minatLain lain2" name="minat" value="Pendidikan" >Pendidikan</label>
+                      <label class="radio lain"><input type="radio" class="minatLain lain2" name="minat" id="radioLain" value="Lainnya" >Bidang Lainnya</label>
+                      <label class="radio lain"><input type="text" disabled class="form-control minatLain" id="minatTxt" name="minatLainTxt" placeholder="ketikkan peminatan anda"></label>
+                    </div>
+                </span>
+                <span id="Aka">
+                  
+                </span>
+                </div>
+                <button type="button" class="mdl-button mdl-js-button mdl-button--raised pull-right" style="background: #43bf44">Kirim</button>
+                <button type="button" onclick="prev()" class="mdl-button mdl-js-button mdl-button--raised pull-right" style="background: #4a95b9;margin-right: 10px;">Kembali</button>
+              </li>
+            </ul>
+            </form>
         </div>
       </div>
 
@@ -90,7 +262,59 @@
 
 </main>
 <script type="text/javascript">
-  $('#frame').show();
+  $('#sub1').hide();
+  $('.lain').hide();
+  $('#Non').hide();
+  $('#Aka').hide();
+
+  $(document).ready(function(){
+    $('input[name="minat"]').click(function(){
+      if ($('input[id="radioLain"]').is(':checked'))
+      {
+        $("#minatTxt").prop('disabled',false);
+      }
+      else{
+        $("#minatTxt").prop('disabled',true);
+      }
+    });
+
+    $('#pilMinat').change(function() {
+        if ($(this).val() === 'Non-Akademik') {
+          $('#Aka').hide();
+          $( "#Non" ).fadeIn( "slow", function() {
+            $(this).show();
+          });
+        } else if($(this).val() === 'Akademik'){
+          $('#Non').hide();
+          $( "#Aka" ).fadeIn( "slow", function() {
+            $(this).show();
+          });
+        }
+    });
+  });
+
+  function klikLain(){
+    $(".lain2").prop('required',true);
+    $( ".lain" ).fadeIn( "slow", function() {
+      $(this).show();
+    });
+  }
+
+  function next(){
+    $('#sub1').hide();
+    $( "#sub2" ).fadeIn( "slow", function() {
+      $(this).show();
+    });
+    return true;
+  }
+
+  function prev(){
+    $('#sub2').hide();
+    $( "#sub1" ).fadeIn( "slow", function() {
+      $(this).show();
+    });
+  }
+
 </script>
 
 
